ff35473444a181c5511c24f271adec1e543c09e7
matheuscarv69 <matheus9126@gmail.com>
2021-07-07 20:38:28 -0400

    :sparkles: feat: Retornando errors no response das requests grpc

--- src/main/kotlin/br/com/zup/edu/FretesGrpcServer.kt
+++ src/main/kotlin/br/com/zup/edu/FretesGrpcServer.kt
@@ -1,7 +1,12 @@
 package br.com.zup.edu
 
+import com.google.protobuf.Any
+import com.google.rpc.Code
+import io.grpc.Status
+import io.grpc.protobuf.StatusProto
 import io.grpc.stub.StreamObserver
 import org.slf4j.LoggerFactory
+import java.lang.IllegalStateException
 import javax.inject.Singleton
 import kotlin.random.Random
 
@@ -14,12 +19,71 @@ class FretesGrpcServer : FretesServiceGrpc.FretesServiceImplBase() {
         request: CalculaFreteRequest?,
         responseObserver: StreamObserver<CalculaFreteResponse>?
     ) {
-        logger.info("Calculando frete para o cep: ${request!!.cep}")
+        logger.info("Calculando frete para o cep: $request")
+
+        val cep = request?.cep
+
+        if (cep == null || cep.isBlank()) {
+            val statusException =
+                Status.INVALID_ARGUMENT
+                    .withDescription("CEP deve ser informado")
+                    .asRuntimeException()
+
+
+            // lancando erro como response para o client
+            responseObserver?.onError(statusException)
+        }
+
+        // verifica se o formato do CEP esta valido
+        if (!cep!!.matches("[0-9]{5}-[0-9]{3}".toRegex())) {
+            val statusException = Status.INVALID_ARGUMENT
+                .withDescription("CEP inválido")
+                .augmentDescription("Formato informado deve ser CEP: 12345-789") // dica para o client
+                .asRuntimeException()
+
+            responseObserver?.onError(statusException)
+        }
+
+        // simulando uma validacao de seguranca
+        if (cep.endsWith("333")) {
+            val statusProto = com.google.rpc.Status.newBuilder()
+                .setCode(Code.PERMISSION_DENIED_VALUE)
+                .setMessage("Usuário não pode acessar esse recurso")
+                .addDetails(
+                    Any.pack(
+                        ErrorDetails.newBuilder()
+                            .setCode(401)
+                            .setMessage("token expirado")
+                            .build()
+                    )
+                )
+                .build()
+
+            val statusException = StatusProto.toStatusRuntimeException(statusProto)
+
+            responseObserver?.onError(statusException)
+        }
+
+        var valor = 0.0
+        try {
+            valor = Random.nextDouble(from = 0.0, until = 140.0) // logica complexa
+            if (valor > 100.0) {
+                throw IllegalStateException("Erro inesperado ao executar lógica de negócio!")
+            }
+
+        } catch (exception: Exception) {
+            responseObserver?.onError(
+                Status.INTERNAL
+                    .withDescription(exception.message)
+                    .withCause(exception) // anexado ao Status de erro, mas nao eh enviado ao Client
+                    .asRuntimeException()
+            )
+        }
 
         val response =
             CalculaFreteResponse.newBuilder()
-                .setCep(request.cep)
-                .setValor(Random.nextDouble(from = 0.0, until = 140.0))
+                .setCep(request?.cep)
+                .setValor(valor)
                 .build()
 
         logger.info("Frete calculado: $response")
